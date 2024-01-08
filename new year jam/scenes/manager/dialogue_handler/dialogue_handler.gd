4f97445b8ab26c9a6bdcba466bd4b11eded1ca4f
angrychill <93102183+angrychill@users.noreply.github.com>
2024-01-08 19:20:11 +0100

    added 50% (adjustable) chance to start dialogue on button press

--- new year jam/scenes/manager/dialogue_handler/dialogue_handler.gd
+++ new year jam/scenes/manager/dialogue_handler/dialogue_handler.gd
@@ -4,7 +4,7 @@ extends Node
 
 @export var dialogue_res : DialogueResource
 @export var title_array : Array[StringName]
-
+@export_range(0, 1.0) var dialogue_chance : float = 0.5
 
 func _unhandled_input(event):
 	pass
@@ -12,15 +12,18 @@ func _unhandled_input(event):
 		#DialogueManager.show_dialogue_balloon(dialogue_res, title_array.pick_random())
 
 func _ready():
+	randomize() # for better rand
 	# receive signal for dialogue
-	# now it's randomly triggered with enter in process
+	# now it has a chance to be randomly triggered on enter
 	pass
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("enter"):
-		print("pressed a")
-		trigger_dialogue(title_array.pick_random())
+		var rand = randf()
+		print(str(rand))
+		if rand < dialogue_chance:
+			trigger_dialogue(title_array.pick_random())
 
 func trigger_dialogue(dialogue):
 	DialogueManager.show_dialogue_balloon(dialogue_res, dialogue)
