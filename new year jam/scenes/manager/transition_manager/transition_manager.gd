5000d4d747c28e4e29309626d5a0a5b3f1a64bd9
manimonji <manirastgar78@gmail.com>
2024-01-11 22:21:54 +0330

    Wormhole completion
    I've renamed scripts and scenes based on naming convention. I've made the scripts a little bit more flexible. I've added transition manager. However , fade in on level start isn't included but it can be done easily but I'm not familiar with main scene

---
+++ new year jam/scenes/manager/transition_manager/transition_manager.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@export var animation_player : AnimationPlayer
+
+func reload_scene_with_transition(transition_name):
+	await play_transition(transition_name).animation_finished
+	get_tree().reload_current_scene()
+
+func load_scene_with_transition(target_scene,transition_name):
+	await play_transition(transition_name).animation_finished
+	get_tree().change_scene_to_packed(target_scene)
+	
+func play_transition(transition_name):
+	animation_player.play(transition_name)
+	return animation_player
